ab3aff989ce61398ad341defeab191aaf4759f25
Mathiaskorre <mathiaskorre@gmail.com>
2026-09-09 13:32:31 +0200

    noget lort

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import Procentregning from "./components/Procentregning";
 import SignIn from "./components/signin";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <NavBar />
       <SignIn />
+      <Procentregning />
     </>
   );
 }
--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -5,7 +5,7 @@ export default function SignIn() {
         <p class="font-bold text-3xl h-10  ">Brugernavn: </p>
         <input
           type="text"
-          placeholder="Indtast brugernavn"
+          placeholder="Indtast brugernav"
           class="border-2 border-black bg-gray-200 rounded-md p-2 m-2"
         />
         <p class="font-bold text-3xl h-10">Adgangskode: </p>
